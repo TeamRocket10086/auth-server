a6caa3e271fd10795b3d4b578ec335ffcddd5c1d
DCH <cdu3@pride.hofstra.edu>
2020-11-25 00:39:49 -0500

    Basic login/register function with a sample test registerToken generator in Application file

--- src/main/java/com/bfs/authserver/AuthServerApplication.java
+++ src/main/java/com/bfs/authserver/AuthServerApplication.java
@@ -1,5 +1,6 @@
 package com.bfs.authserver;
 
+import com.bfs.authserver.security.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServerApplication {
 
     public static void main(String[] args) {
+
+        //test register get register token
+        String spersonId = String.valueOf(2);
+        System.out.println(JwtUtil.generateRegisterToken("signingKeyemployee", spersonId, "test2@1234.com"));
+
         SpringApplication.run(AuthServerApplication.class, args);
     }
 
